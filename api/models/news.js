67981e150fd41c5e98c03d501dbc93e18f7fae70
Feer Aleksandr <feer@retailsuite.io>
2018-09-16 02:18:55 +0300

    add models

---
+++ api/models/news.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const newsShema = new Schema({
+  name: {
+    type: String,
+    required: [true, 'Укажите описание работы'],
+  },
+  tech: {
+    type: String,
+    required: [true, 'Укажите используемые технологии'],
+  },
+  link: {
+    type: String,
+    required: [true, 'Укажите скилы'],
+  },
+  picture: {
+    type: String,
+    required: [true, 'Загрузите картинку'],
+  },
+});
+
+mongoose.model('news', newsShema);
